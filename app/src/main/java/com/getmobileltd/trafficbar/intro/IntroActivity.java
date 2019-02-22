a4396a836e184ba5d75485808a802c43e6039403
Tobiloba Adejumo <eniolaplus08@gmail.com>
2019-02-22 14:18:08 +0100

    Temp intro layout created

---
+++ app/src/main/java/com/getmobileltd/trafficbar/intro/IntroActivity.java
@@ -0,0 +1,15 @@
+package com.getmobileltd.trafficbar.intro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.getmobileltd.trafficbar.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
