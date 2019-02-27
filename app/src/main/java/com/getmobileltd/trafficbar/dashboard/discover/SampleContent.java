879a5de6e3a6fe8d89ff31f3dec3a7471e65cacb
Tobiloba Adejumo <eniolaplus08@gmail.com>
2019-02-27 13:46:34 +0100

    Discover fragment working demo

---
+++ app/src/main/java/com/getmobileltd/trafficbar/dashboard/discover/SampleContent.java
@@ -0,0 +1,40 @@
+/*
+ * *
+ * Creator: Tobiloba Adejumo on 2/27/19 12:22 PM Last modified: 2/27/19 12:22 PM Copyright: All rights reserved Ⓒ 2019
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *    Unless required by applicable law or agreed to in writing, software distributed under
+ *    the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ *    OF ANY KIND, either express or implied. See the License for the specific language governing
+ *    permissions and limitations under the License.
+ * /
+ */
+
+package com.getmobileltd.trafficbar.dashboard.discover;
+
+import com.getmobileltd.trafficbar.dashboard.discover.model.DiscoveryModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by themavencoder on 27,February,2019
+ */
+public class SampleContent {
+
+    public static List<DiscoveryModel> RESTAURANT = new ArrayList<>();
+
+    static {
+        DiscoveryModel model1 = new DiscoveryModel("15 Montgomerry Road,", "Igando, Lagos","4.1");
+        DiscoveryModel model2 = new DiscoveryModel("15 Victoria Garden City,", "Igando, Lagos","4.2");
+        DiscoveryModel model3 = new DiscoveryModel("15 Assam Esso Street,", "Igando, Lagos","4.3");
+        RESTAURANT.add(model1);
+        RESTAURANT.add(model2);
+        RESTAURANT.add(model3);
+        RESTAURANT.add(model1);
+        RESTAURANT.add(model2);
+        RESTAURANT.add(model3);
+    }
+}
