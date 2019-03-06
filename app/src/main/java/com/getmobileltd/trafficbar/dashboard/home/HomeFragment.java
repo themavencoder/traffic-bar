04445cc8e9bd05d767f675267e4c0ecc32c229f5
Tobiloba Adejumo <eniolaplus08@gmail.com>
2019-03-06 13:58:06 +0100

    Order table activity created

--- app/src/main/java/com/getmobileltd/trafficbar/dashboard/home/HomeFragment.java
+++ app/src/main/java/com/getmobileltd/trafficbar/dashboard/home/HomeFragment.java
@@ -15,9 +15,11 @@
 package com.getmobileltd.trafficbar.dashboard.home;
 
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -35,13 +37,14 @@ import com.getmobileltd.trafficbar.dashboard.home.food.FoodAdapter;
 import com.getmobileltd.trafficbar.dashboard.home.food.FoodModel;
 import com.getmobileltd.trafficbar.dashboard.home.trend.TrendAdapter;
 import com.getmobileltd.trafficbar.dashboard.home.trend.TrendModel;
+import com.getmobileltd.trafficbar.orderfood.foodlist.ListAvailableFoodActivity;
 
 import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
     private RecyclerView recyclerViewTrend, recyclerViewFood, recyclerViewDrink;
     private List<TrendModel> modelList = SampleContent.MYTRENDS;
     private List<FoodModel> modelListFood = SampleContent.MYFOOD;
@@ -50,6 +53,7 @@ public class HomeFragment extends Fragment {
     private FoodAdapter adapterFood;
     private DrinksAdapter adapterDrink;
     private View v;
+    private CardView cardViewFood;
 
 
     public HomeFragment() {
@@ -62,30 +66,43 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        v =  inflater.inflate(R.layout.fragment_home, container, false);
-        recyclerViewTrend = v.findViewById(R.id.recycler_view_trending);
-        recyclerViewFood = v.findViewById(R.id.recycler_view_food);
-        recyclerViewDrink = v.findViewById(R.id.recycler_view_drinks);
-
-        adapterTrend = new TrendAdapter(getActivity(),modelList);
-        adapterFood = new FoodAdapter(getActivity(),modelListFood);
-        adapterDrink = new DrinksAdapter(getActivity(),modelListDrinks);
-
-
-
-        recyclerViewTrend.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL,false));
-        recyclerViewTrend.setAdapter(adapterTrend);
-
-        recyclerViewFood.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL,false));
-        recyclerViewFood.setAdapter(adapterFood);
-
-        recyclerViewDrink.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL,false));
-        recyclerViewDrink.setAdapter(adapterDrink);
+        v = inflater.inflate(R.layout.fragment_home, container, false);
+        init(v);
 
         return v;
     }
+
     public int dpToPx(int dp) {
         Resources r = getResources();
         return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.cardview_order_for_food:
+                startActivity(new Intent(getActivity(), ListAvailableFoodActivity.class));
+
+        }
+
+
+    }
+
+    private void init(View v) {
+        cardViewFood = v.findViewById(R.id.cardview_order_for_food);
+        cardViewFood.setOnClickListener(this);
+        recyclerViewTrend = v.findViewById(R.id.recycler_view_trending);
+        recyclerViewFood = v.findViewById(R.id.recycler_view_food);
+        recyclerViewDrink = v.findViewById(R.id.recycler_view_drinks);
+        adapterTrend = new TrendAdapter(getActivity(), modelList);
+        adapterFood = new FoodAdapter(getActivity(), modelListFood);
+        adapterDrink = new DrinksAdapter(getActivity(), modelListDrinks);
+        recyclerViewTrend.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        recyclerViewTrend.setAdapter(adapterTrend);
+        recyclerViewFood.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        recyclerViewFood.setAdapter(adapterFood);
+        recyclerViewDrink.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        recyclerViewDrink.setAdapter(adapterDrink);
+
     }
 }
